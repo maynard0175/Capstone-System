d79513a64a172e08b1565e33eb675344caeb8fe4
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-10-10 20:44:26 +0800

    added the dashboard page content and styles

--- app/views/student/index.php
+++ app/views/student/index.php
@@ -22,7 +22,9 @@ if ($_SESSION['role'] != 'student') {
     <title>Student</title>
     <script src="../../assets/tailwind/tailwind.js"></script>
     <link rel="stylesheet" href="../../assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/css/student.css">
     <script src="../../assets/bootstrap/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -30,14 +32,38 @@ if ($_SESSION['role'] != 'student') {
         <p>Student session started.....</p>
         <button onclick="logout()" class="bg-[#007BFF] text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Logout</button>
     </div> -->
-    <div class="vh-100 container-fluid d-flex align-items-center justify-content-center">
+    <div class=" vh-100 container-fluid d-flex align-items-center justify-content-center dashboard-container">
         <div class="container">
             <div class="container text-center d-flex align-items-center justify-content-center">
-                <img src="../../assets/img/logo.png" alt="" class="img-fluid text-center">
+                <img src="../../assets/img/logo.png" alt="" class="img-fluid text-center logo">
             </div>
 
-            <h2 class="text-center mt-2 mb-3 fw-bold">Dashboard Overview</h2>
+            <h2 class="text-center my-4">Dashboard Overview</h2>
 
+           <div class="container d-flex align-items-center justify-content-center">
+                <div class="container mb-4 d-flex align-items-center justify-content-center profile-container">
+                    <div class="container d-flex align-content-between justify-content-between profile-card p-3">
+                        <h2 class="my-auto">PROFILE</h2>
+                        <div class="icon">
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                    </div>
+                </div>
+           </div>
+           <div class="container d-flex align-items-center justify-content-center">
+                <div class="container course-container">
+                    <div class="container d-flex align-content-between justify-content-between course-card p-3">
+                        <h2 class="my-auto">COURSE</h2>
+                        <div class="icon">
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                    </div>
+                </div>
+           </div>
+           
+            <div class="mt-5 container d-flex align-items-center justify-content-center">
+                <img src="../../assets/img/infotech.png" alt="" class="img-fluid infotech-img">
+            </div>
 
         </div>
     </div>
