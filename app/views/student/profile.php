2b978025f8cdb571ee405625c84bf45092d4c025
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-10-11 12:54:52 +0800

    added the content for the profile page

--- app/views/student/profile.php
+++ app/views/student/profile.php
@@ -4,8 +4,152 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <link rel="stylesheet" href="../../assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/css/student.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="../../assets/bootstrap/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../assets/icon/bootstrap-icons.min.css">
 </head>
 <body>
-    
+    <nav class="navbar container-fluid w-100 navbar-expand-lg dashboard-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">DASHBOARD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">PROFILE</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">GRADES</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Link
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li> -->
+                <li class="nav-item">
+                <a class="nav-link" href="#">REGISTRATION</a>
+                </li>
+            </ul>
+            <form class="d-flex align-items-center justify-content-center">
+                <div class="icon my-auto my-auto">
+                <i class="bi bi-bell"></i>
+                </div>
+                <div class="user-info d-flex align-items-center justify-content-center my-auto">
+                    <div class="icon my-auto">
+                    <i class="bi bi-person-circle"></i>
+                    </div>
+                    <p class="my-auto">Maynard Bihasa</p>
+                </div>
+            </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid py-3 px-5 bg-secondary">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="container bg-white p-2">
+                    <div class="text-center">
+                        <img src="../../assets/img/logo.png" alt="" class="logo">
+                    </div>
+                    <h5 class="text-center">Maynard Bihasa</h5>
+                    <h6 class="text-secondary text-center">22-01-0694</h6>
+                    <h5 class="text-secondary text-center">BSIT-AP-3</h5>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Status:</p>
+                        <span>Not Defined</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Birthdate:</p>
+                        <span>January 1, 2001</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Mobile No.</p>
+                        <span>09221234567</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold">Email</p>
+                        <span>juandelacruz@gmail.com</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Birthplace</p>
+                        <span>Silang, Cavite</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Sex</p>
+                        <span>Male</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Civil Status</p>
+                        <span>Single</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-7">
+            <div class="container bg-white p-2 mt-4">
+                    <!-- <div class="text-center">
+                        <img src="../../assets/img/logo.png" alt="" class="logo">
+                    </div> -->
+                    <h5 class="text-center">Maynard Bihasa</h5>
+                    <h6 class="text-secondary text-center">22-01-0694</h6>
+                    <h5 class="text-secondary text-center">BSIT-AP-3</h5>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Status:</p>
+                        <span>Not Defined</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Birthdate:</p>
+                        <span>January 1, 2001</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Mobile No.</p>
+                        <span>09221234567</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold">Email</p>
+                        <span>juandelacruz@gmail.com</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Birthplace</p>
+                        <span>Silang, Cavite</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Sex</p>
+                        <span>Male</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-content-between justify-content-between py-0 my-0">
+                        <p class="fw-semibold my-0 py-0">Civil Status</p>
+                        <span>Single</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="container-fluid d-flex align-items-center justify-content-center p-5">
+        <h2 class="text-center">This is a profile</h2>
+    </div> -->
 </body>
 </html>
