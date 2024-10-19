ab7ad2220b0f113739454800948531a3e9e05588
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-10-20 00:23:11 +0800

    updated the admin dashboard

--- app/assets/js/admin.js
+++ app/assets/js/admin.js
@@ -1,38 +1,81 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-   
-    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId)
-    
-    // Validate that all variables exist
-    if(toggle && nav && bodypd && headerpd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    nav.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                toggle.classList.toggle('bx-x')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
     }
-    }
-    
-    showNavbar('header-toggle','nav-bar','body-pd','header')
-    
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+
     /*===== LINK ACTIVE =====*/
     const linkColor = document.querySelectorAll('.nav_link')
-    
-    function colorLink(){
-    if(linkColor){
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach(l => l.classList.remove('active'))
+            this.classList.add('active')
+        }
     }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+
+    // Your code to run since DOM is loaded and ready
+
+    // Function to handle submenu toggle
+    function toggleSubmenus(clickedMenu) {
+        const allMenus = document.querySelectorAll('.collapse');
+
+        // Collapse all menus except the clicked one
+        allMenus.forEach(menu => {
+            const parentLink = document.querySelector(`a[href="#${menu.id}"]`);
+            if (parentLink !== clickedMenu) {
+                menu.classList.remove('show');
+                parentLink.setAttribute('aria-expanded', 'false');
+            }
+        });
+
+        // Check if the clicked menu is already expanded
+        const isExpanded = clickedMenu.getAttribute('aria-expanded') === 'true';
+
+        // Toggle the clicked menu
+        if (!isExpanded) {
+            const currentMenu = clickedMenu.getAttribute('href');
+            document.getElementById(currentMenu.substring(1)).classList.add('show');
+            clickedMenu.setAttribute('aria-expanded', 'true');
+        } else {
+            clickedMenu.setAttribute('aria-expanded', 'false');
+            const currentMenu = clickedMenu.getAttribute('href');
+            document.getElementById(currentMenu.substring(1)).classList.remove('show');
+        }
     }
-    linkColor.forEach(l=> l.addEventListener('click', colorLink))
-    
-     // Your code to run since DOM is loaded and ready
+
+    // Event listener for dashboard link
+    document.getElementById('dashboardLink').addEventListener('click', function () {
+        toggleSubmenus(this);
     });
+
+    // Event listener for each collapsible link
+    document.querySelectorAll('.nav_link[data-bs-toggle="collapse"]').forEach(link => {
+        link.addEventListener('click', function () {
+            // Clicking the same menu will collapse it
+            toggleSubmenus(this);
+        });
+    });
+});
--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -20,17 +20,102 @@
 <body id="body-pd">
     <header class="header" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-        <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+        <div class="ms-5 text-uppercase fw-bolder">Enrollment System Management</div>
+        <div class="ms-auto">Info Tech S-Admin</div>
+        <div class="header_img ms-2"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
     </header>
-    <div class="l-navbar" id="nav-bar">
-        <nav class="nav">
-            <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a>
-                <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> </div>
-            </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
-        </nav>
-    </div>
+    <div class="l-navbar vh-100" id="nav-bar">
+    <nav class="nav">
+        <div>
+            <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">InfoTech</span> </a>
+            <div class="nav_list">
+                <a href="#" class="nav_link active" id="dashboardLink"> 
+                    <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> 
+                </a>
+                
+                <!-- Users Link with Collapsible Submenu -->
+                <a href="#userSubmenu" class="nav_link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="userSubmenu">
+                    <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span>
+                </a>
+                <div class="collapse" id="userSubmenu">
+                    <ul class="list-unstyled ps-4">
+                        <li>
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> Add User</a>
+                        </li>
+                        <li class=""> <!-- Small margin below -->
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> View Users</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Messages Link with Collapsible Submenu -->
+                <a href="#messageSubmenu" class="nav_link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="messageSubmenu">
+                    <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span>
+                </a>
+                <div class="collapse" id="messageSubmenu">
+                    <ul class="list-unstyled ps-4">
+                        <li>
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> Send Message</a>
+                        </li>
+                        <li class=""> <!-- Small margin below -->
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> View Messages</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Bookmark Link with Collapsible Submenu -->
+                <a href="#bookmarkSubmenu" class="nav_link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="bookmarkSubmenu">
+                    <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span>
+                </a>
+                <div class="collapse" id="bookmarkSubmenu">
+                    <ul class="list-unstyled ps-4">
+                        <li>
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> Add Bookmark</a>
+                        </li>
+                        <li class=""> <!-- Small margin below -->
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> View Bookmarks</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Files Link with Collapsible Submenu -->
+                <a href="#fileSubmenu" class="nav_link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="fileSubmenu">
+                    <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span>
+                </a>
+                <div class="collapse" id="fileSubmenu">
+                    <ul class="list-unstyled ps-4">
+                        <li>
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> Upload File</a>
+                        </li>
+                        <li class=""> <!-- Small margin below -->
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> View Files</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Stats Link with Collapsible Submenu -->
+                <a href="#statsSubmenu" class="nav_link" data-bs-toggle="collapse" aria-expanded="false" aria-controls="statsSubmenu">
+                    <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span>
+                </a>
+                <div class="collapse" id="statsSubmenu">
+                    <ul class="list-unstyled ps-4">
+                        <li>
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> View Stats</a>
+                        </li>
+                        <li class="mb-2"> <!-- Small margin below -->
+                            <a href="#" class="nav_link"><i class='bx bx-chevron-right nav_icon'></i> Stat Reports</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <a href="../../logout.php" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
+    </nav>
+</div>
+
+
     <!--Container Main start-->
-    <div class="mt-5 height-100 p-5 bg-danger container-fluid">
+    <div class="mt-5 height-100 vh-100 p-5 container-fluid bg-secondary">
         <h2>Main Components</h2>
     </div>
     <!--Container Main end-->
